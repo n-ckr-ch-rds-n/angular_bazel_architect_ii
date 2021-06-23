9864afbbedc97d84fc84efd0b9c2bd1e6424515a
nick_richardson <nick.richardson@rezonence.com>
2021-06-23 15:49:03 +0100

    Add arrow component

--- src/app/arrow/arrow.component.ts
+++ src/app/arrow/arrow.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, HostBinding, Input, OnInit} from "@angular/core";
+import {ArrowDirection} from "./arrow.direction";
+import {Theme} from "../config/theme";
 
 @Component({
-  selector: 'app-arrow',
-  templateUrl: './arrow.component.html',
-  styleUrls: ['./arrow.component.scss']
+  selector: "app-arrow",
+  templateUrl: "./arrow.component.html",
+  styleUrls: ["./arrow.component.scss"]
 })
 export class ArrowComponent implements OnInit {
 
-  constructor() { }
+  ArrowDirection = ArrowDirection;
 
-  ngOnInit(): void {
+  @Input()
+  theme: Theme;
+
+  @Input()
+  direction: ArrowDirection;
+
+  get height(): string {
+    return `${this.elementRef.nativeElement.offsetHeight}px`;
   }
 
+  get width(): string {
+    return `${this.elementRef.nativeElement.offsetWidth}px`;
+  }
+
+  constructor(public elementRef: ElementRef) {
+
+  }
+
+  ngOnInit() {
+  }
 }
