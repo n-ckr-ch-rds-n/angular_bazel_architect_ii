716d044322d537adb2c868acbd3236b01024a745
nick_richardson <nick.richardson@rezonence.com>
2021-06-23 14:26:24 +0100

    Add banner component and cdk to deps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,25 @@
 import {NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
-
 import {AppRoutingModule} from "./app-routing.module";
 import {AppComponent} from "./app.component";
-import { ContainerComponent } from "./container/container.component";
+import {ContainerComponent} from "./container/container.component";
+import {BannerComponent} from "./banner/banner.component";
+import {FlexLayoutModule} from "@angular/flex-layout";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ContainerComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FrontendLibModule,
-  ],
-  bootstrap: [
-    AppComponent
-  ]
+    declarations: [
+        AppComponent,
+        ContainerComponent,
+        BannerComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FlexLayoutModule,
+    ],
+    bootstrap: [
+        AppComponent
+    ]
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from "@angular/core";
+import {FreeWallConfig} from "@rezonence/core/config-extractor/freewall/freewall.config";
+import {FreeWallData} from "@rezonence/sdk";
+import {MicrositeService} from "../microsite.service";
+import {Theme} from "../config/theme";
 
 @Component({
-  selector: 'app-banner',
-  templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.scss']
+  selector: "app-banner",
+  templateUrl: "./banner.component.html",
+  styleUrls: ["./banner.component.scss"]
 })
 export class BannerComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  data: FreeWallData;
 
-  ngOnInit(): void {
+  @Input()
+  theme: Theme;
+
+  @Input()
+  freewallConfig: FreeWallConfig;
+
+  constructor(private micrositeService: MicrositeService) {
+  }
+
+  ngOnInit() {
+    this.micrositeService.notifyReady();
   }
 
 }
