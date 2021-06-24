dde1601145aeb925d89c7d2791807dd6b8fd543f
nick_richardson <nick.richardson@rezonence.com>
2021-06-24 16:24:28 +0100

    Test answers component renders

---
+++ multi-video-microsite/src/app/answers/answers.component.spec.ts
@@ -0,0 +1,27 @@
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+
+import { AnswersComponent } from "./answers.component";
+
+describe("AnswersComponent", () => {
+  let component: AnswersComponent;
+  let fixture: ComponentFixture<AnswersComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ AnswersComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AnswersComponent);
+    component = fixture.componentInstance;
+    const mockQuestion = {que: "foo?", answers: []};
+    component.question = mockQuestion;
+    fixture.detectChanges();
+  });
+
+  it("should create", () => {
+    expect(component).toBeTruthy();
+  });
+});
