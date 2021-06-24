e7d60e87565c69a820bd19f1fe1f6190f9d13528
nick_richardson <nick.richardson@rezonence.com>
2021-06-24 17:08:09 +0100

    Q & A and retry

---
+++ multi-video-microsite/src/app/question-and-answers/question-and-answers.component.spec.ts
@@ -0,0 +1,28 @@
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+
+import { QuestionAndAnswersComponent } from "./question-and-answers.component";
+import {Question} from "@rezonence/core/config-extractor/freewall/question";
+
+describe("QuestionAndAnswersComponent", () => {
+  let component: QuestionAndAnswersComponent;
+  let fixture: ComponentFixture<QuestionAndAnswersComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ QuestionAndAnswersComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(QuestionAndAnswersComponent);
+    component = fixture.componentInstance;
+    component.question = {que: "foo"} as Question;
+    component.theme = {};
+    fixture.detectChanges();
+  });
+
+  it("should create", () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ multi-video-microsite/src/app/retry/retry.component.spec.ts
@@ -0,0 +1,26 @@
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+
+import { RetryComponent } from "./retry.component";
+
+describe("RetryComponent", () => {
+  let component: RetryComponent;
+  let fixture: ComponentFixture<RetryComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ RetryComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RetryComponent);
+    component = fixture.componentInstance;
+    component.theme = {buttonColour: "black", buttonTextColour: "pink"};
+    fixture.detectChanges();
+  });
+
+  it("should create", () => {
+    expect(component).toBeTruthy();
+  });
+});
