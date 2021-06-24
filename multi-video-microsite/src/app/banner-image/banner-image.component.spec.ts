7c3eeda185d1ac557687e80f450fdbf28508e610
nick_richardson <nick.richardson@rezonence.com>
2021-06-24 16:01:41 +0100

    Configure test setup

--- multi-video-microsite/src/app/banner-image/banner-image.component.spec.ts
+++ multi-video-microsite/src/app/banner-image/banner-image.component.spec.ts
@@ -1,25 +1,28 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from "@angular/core/testing";
+import {BannerImageComponent} from "./banner-image.component";
+import {LinkPipe} from "../link.pipe";
 
-import { BannerImageComponent } from './banner-image.component';
+describe("BannerComponent", () => {
+    let component: BannerImageComponent;
+    let fixture: ComponentFixture<BannerImageComponent>;
 
-describe('BannerImageComponent', () => {
-  let component: BannerImageComponent;
-  let fixture: ComponentFixture<BannerImageComponent>;
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [
+                BannerImageComponent,
+                LinkPipe
+            ]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ BannerImageComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(BannerImageComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(BannerImageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it("should create", () => {
+        expect(component).toBeTruthy();
+    });
 });
